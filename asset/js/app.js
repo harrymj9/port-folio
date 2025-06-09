ac52b62703386c46e91e1207ebc5914de71612f8
Johoane <axelmonde7@gmail.com>
2025-06-10 00:51:35 +0200

    first commit

---
+++ asset/js/app.js
@@ -0,0 +1,41 @@
+/* ========== ANIMATED TEXT  ==========
+*/
+  const words = ["WEB DESIGNER", "FRONT-END", "BACK-END", "MACHINE LEARNING"];
+  let i = 0;
+  let j = 0;
+  let currentWord = '';
+  let isDeleting = false;
+  const speed = 100;
+  const typingElement = document.getElementById('typing');
+
+  function type() {
+    currentWord = words[i];
+    typingElement.innerHTML = currentWord.substring(0, j);
+
+    if (!isDeleting && j < currentWord.length) {
+      j++;
+      setTimeout(type, speed);
+    } else if (isDeleting && j > 0) {
+      j--;
+      setTimeout(type, speed / 2);
+    } else {
+      isDeleting = !isDeleting;
+      if (!isDeleting) {
+        i = (i + 1) % words.length;
+      }
+      setTimeout(type, 1000);
+    }
+  }
+
+  type();
+
+/* ========== BACK TO TOP BUTTON ==========*/
+  const backToTopBtn = document.getElementById('backToTop');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    backToTopBtn.classList.add('show');
+  } else {
+    backToTopBtn.classList.remove('show');
+  }
+});
